b2e4e44e15637d7e0c3766a773ef640f3a7289ed
Facu Salmerón <Facu Salmern>
2018-06-11 21:16:59 -0300

    -

--- app/src/main/java/com/encuestando/salmeron/facundo/encuestandofcm/MainActivity.java
+++ app/src/main/java/com/encuestando/salmeron/facundo/encuestandofcm/MainActivity.java
@@ -1,13 +1,27 @@
 package com.encuestando.salmeron.facundo.encuestandofcm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    CardView boton_registrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        boton_registrar = (CardView) findViewById(R.id.register_button);
+        boton_registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent register_intent = new Intent(MainActivity.this, RegisterActivity.class);
+                MainActivity.this.startActivity(register_intent);
+            }
+        });
     }
 }
