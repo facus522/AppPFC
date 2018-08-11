c7f0dddf212a0014fa8790577b0f33a91420f78d
Facu Salmerón <Facu Salmern>
2018-08-11 17:38:11 -0300

    -

--- app/src/main/java/com/encuestando/salmeron/facundo/encuestandofcm/InfoNoticiasEspecialActivity.java
+++ app/src/main/java/com/encuestando/salmeron/facundo/encuestandofcm/InfoNoticiasEspecialActivity.java
@@ -3,6 +3,8 @@ package com.encuestando.salmeron.facundo.encuestandofcm;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -10,12 +12,14 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
+
 
 public class InfoNoticiasEspecialActivity extends AppCompatActivity implements HttpAsyncTaskInterface{
 
@@ -24,18 +28,28 @@ public class InfoNoticiasEspecialActivity extends AppCompatActivity implements H
     private HttpAsyncTask httpAsyncTask;
     private String urlInfo;
     private Toolbar toolbar;
+    private FloatingActionButton agregarInfoBoton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info_noticias_especial_activity);
         listViewInfoNoticias = (ListView) findViewById(R.id.listaInfoNoticiasEspecial);
+        agregarInfoBoton = (FloatingActionButton) findViewById(R.id.boton_agregar_info);
         toolbar = (Toolbar) findViewById(R.id.toolbar_info_especial);
         toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_back));
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 InfoNoticiasEspecialActivity.this.finish();
+            }
+        });
+
+        agregarInfoBoton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(InfoNoticiasEspecialActivity.this, "Agregar Info", Toast.LENGTH_LONG).show();
             }
         });
 
@@ -61,38 +75,40 @@ public class InfoNoticiasEspecialActivity extends AppCompatActivity implements H
             int[] widgetIds = {R.id.titulo_info_normal, R.id.subtitulo_info_normal};
             SimpleAdapter crsAdapter = new SimpleAdapter(InfoNoticiasEspecialActivity.this, crsList, R.layout.info_noticias_item, keys,widgetIds);
 
-
-
-
             listViewInfoNoticias.setAdapter(crsAdapter);
 
         } else{
-            final AlertDialog.Builder alertDialog = new AlertDialog.Builder(InfoNoticiasEspecialActivity.this);
-            alertDialog.setTitle("Sin Datos");
-            alertDialog.setMessage("Actualmente no se encuentran cargadas Informaciones y Noticias.");
-            alertDialog.setIcon(R.drawable.ic_action_error);
-            alertDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.cancel();
-                    InfoNoticiasEspecialActivity.this.finish();
-                }
-            });
-            alertDialog.show();
+            Toast.makeText(InfoNoticiasEspecialActivity.this, "Actualmente no se encuentran cargadas Informaciones y Noticias.", Toast.LENGTH_LONG).show();
         }
 
         listViewInfoNoticias.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                urlInfo = "";
-                if (!infoNoticiasDto.get(i).getUrl().substring(0,4).equals("http")){
-                    urlInfo += "http://" + infoNoticiasDto.get(i).getUrl();
-                } else{
-                    urlInfo = infoNoticiasDto.get(i).getUrl();
-                }
+            public void onItemClick(AdapterView<?> adapterView, View view, final int i, long l) {
+                final AlertDialog.Builder alertDialog = new AlertDialog.Builder(InfoNoticiasEspecialActivity.this);
+                alertDialog.setTitle("Confirme opción a realizar");
+                alertDialog.setMessage("¿Desea ingresar a la información o eliminarla?");
+                alertDialog.setPositiveButton("Ver URL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        urlInfo = "";
+                        if (!infoNoticiasDto.get(i).getUrl().substring(0,4).equals("http")){
+                            urlInfo += "http://" + infoNoticiasDto.get(i).getUrl();
+                        } else{
+                            urlInfo = infoNoticiasDto.get(i).getUrl();
+                        }
 
-                Intent web_info_intent = new Intent(InfoNoticiasEspecialActivity.this, WebViewNormalActivity.class).putExtra("urlInfo", urlInfo);
-                InfoNoticiasEspecialActivity.this.startActivity(web_info_intent);
+                        Intent web_info_intent = new Intent(InfoNoticiasEspecialActivity.this, WebViewNormalActivity.class).putExtra("urlInfo", urlInfo);
+                        InfoNoticiasEspecialActivity.this.startActivity(web_info_intent);
+                    }
+                });
+                alertDialog.setNegativeButton("Eliminar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                        Toast.makeText(InfoNoticiasEspecialActivity.this, "Eliminar.", Toast.LENGTH_LONG).show();
+                    }
+                });
+                alertDialog.show();
             }
         });
 
